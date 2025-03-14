91dffe29ccf127641b17fbd6100216b4703f969b
skipper-108 <sumitiiitbh554149@gmail.com>
2025-03-14 21:45:52 +0530

    first commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,5 @@
 "use client";
-
+/* eslint-disable */
 import { motion } from "framer-motion";
 import { socials } from "../constants";
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,5 @@
 "use client";
-
+/* eslint-disable */
 import { motion } from "framer-motion";
 
 import styles from "../styles";
